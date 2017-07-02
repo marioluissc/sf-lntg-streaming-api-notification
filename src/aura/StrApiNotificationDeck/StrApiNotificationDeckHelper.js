cfd099570b34a2de9cca98c9d188915e4a10fc66
Mario Citadin <mario@forceadm.com>
2017-07-03 09:56:53 +1000

    Update StrApiNotificationDeckHelper.js

--- src/aura/StrApiNotificationDeck/StrApiNotificationDeckHelper.js
+++ src/aura/StrApiNotificationDeck/StrApiNotificationDeckHelper.js
@@ -5,23 +5,16 @@
         var title = event.getParam('title');
         var content = event.getParam('content') || '';
         var style = event.getParam('style') || '';
-		var hideAfter = component.get('v.hideAfter');
+	var hideAfter = component.get('v.hideAfter');
 
-        $A.createComponent(
-            ns+':UINotification',
-            {
-                title: title,
-                content: content,
-                style: style,
-                type: notificationType,
-                hideAfter: hideAfter,
-            },
-            function(cmp) {
-                var body = component.get("v.body");
-                body.push(cmp);
-                component.set("v.body", body);
-                
-		});
-
-	}
+       	var toastEvent = $A.get("e.force:showToast");
+        console.log('Create Toast');
+        toastEvent.setParams({
+            "title": title,
+            "message": content,
+            "duration": hideAfter*1000,
+            "type" : style,
+	    "mode" : notificationType
+        });
+   	toastEvent.fire();
 })
